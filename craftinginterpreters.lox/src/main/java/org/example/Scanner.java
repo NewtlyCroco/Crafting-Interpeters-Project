604deb14bb2f5ae4665db9456c7443f2657f7888
NewtlyCroco <willmitchellbusinessva@gmail.com>
2025-01-12 12:35:35 +0200

    MORE SCANNER UPDATES
    We are getting into more complex scanning now, in our creation of tokens! Later we will get even more complex with literals!

--- craftinginterpreters.lox/src/main/java/org/example/Scanner.java
+++ craftinginterpreters.lox/src/main/java/org/example/Scanner.java
@@ -43,13 +43,50 @@ public class Scanner {
             case '-': addToken(MINUS); break;
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+            case '/':
+                if (match('/')){
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }
+                else{
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            case '\n':
+                line++;
+                break;//very important, when we see a new line character we want to move the line so that our start, end, current can do its job right and we can properly track errors as well in our tokens!
 
             default:
                 Main.error(line, "Unexpected Character");
                 break;//super importatnt to our program and in conjunciton with the advance method, we keep scanning until the whole source file is scanned so that user does not have to play whack a mole with the errors they are receiveing and can rather see all the errors at once produced by the compiler!
 
         }
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);//just a mini version of our match, just looking at what the current character is ahead of our currently scanning character.
+
+    }
+
+    private boolean match(char expected){
+        if(isAtEnd()) return false;
+        if (source.charAt(current) == expected) return false;
+
+        current++;
+        return true;//looks confusing but is not! our current "pointer" sits on the character after the one we are actually looking at as seen in the advance method, so by looking and matching the actual current variable pointer to what we want to match, we are looking ahead to see if the next character is something we want it to be!
     }
 
     private char advance(){
