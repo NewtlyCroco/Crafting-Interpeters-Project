e3f3f2abe2687fd2f004e819863889cd5b6432ab
NewtlyCroco <willmitchellbusinessva@gmail.com>
2025-03-03 00:00:50 +0200

    Script for Syntax Tree
    The syntax tree is a weird section of code that looks like it just is sitting data, when in reality it exists that way to act as a bridge between our lower level lexeme language with just tokens to expressions of logic, and the joining of tokens into recongnizable code statements we are looking at productions, and the trees we can make of logic using our tokens and lexems! Howe amazing, still fuzzy on some stuff about this language form conversion but we will get there

--- craftinginterpreters.lox/src/main/tool/GenerateAST.java
+++ craftinginterpreters.lox/src/main/tool/GenerateAST.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class GenerateAST {
@@ -10,10 +11,34 @@ public class GenerateAST {
     }
     String outputDir = args[0];
     defineAst(outputDir, "Expr", Arrays.asList(
-        "Binary    : Expr left, Token operator, Expr right",
+       "Binary     : Expr left, Token operator, Expr right",
             "Grouping   : Expr expression",
             "Literal    : Object Value",
             "Unary      : Token operator, Expr right"
     ));
-   } 
+   }
+   
+
+   private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+    String path = outputDir + "/" + baseName + ".java";
+    PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+    writer.println("package craftinginterpeters.lox;");
+    writer.println();
+    writer.println("import java.util.List;");
+    writer.println();
+    writer.println("abstract class " + baseName + " {");
+    for(String type: types){
+        String className = type.split(":")[0].trim();
+        String fields = type.split(":")[1].trim();
+        defineType(writer, baseName, className, fields);
+    }
+    writer.println("}");
+    writer.close(); 
+   }
+   private static void defineType(PrinterWriter writer, String baseName, String className, String fieldList){
+    writer.println("  static class " + className + " extends " + baseName + " {");
+   }//this is where i stopped for the night!
+
+
 }
