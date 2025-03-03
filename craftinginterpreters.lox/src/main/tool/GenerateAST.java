3bb1dd9f92ba7a54c98263f67583b3762cb77503
NewtlyCroco <willmitchellbusinessva@gmail.com>
2025-03-04 00:10:36 +0200

    Finished the scripting tool
    to create our syntax tree classes, for all productions that we need to create a more logical language format for code generation

--- craftinginterpreters.lox/src/main/tool/GenerateAST.java
+++ craftinginterpreters.lox/src/main/tool/GenerateAST.java
@@ -10,7 +10,7 @@ public class GenerateAST {
 
     }
     String outputDir = args[0];
-    defineAst(outputDir, "Expr", Arrays.asList(
+    defineAst(outputDir, "Expr", Arrays.asList(//how is this different from the tokens we made with the scanner?
        "Binary     : Expr left, Token operator, Expr right",
             "Grouping   : Expr expression",
             "Literal    : Object Value",
@@ -27,7 +27,7 @@ public class GenerateAST {
     writer.println();
     writer.println("import java.util.List;");
     writer.println();
-    writer.println("abstract class " + baseName + " {");
+    writer.println("abstract class " + baseName + " {");// in our generate we are looking at splitting the actual productions we have defined and the even more spesific types, like binary etc.
     for(String type: types){
         String className = type.split(":")[0].trim();
         String fields = type.split(":")[1].trim();
@@ -36,9 +36,31 @@ public class GenerateAST {
     writer.println("}");
     writer.close(); 
    }
+
+
    private static void defineType(PrinterWriter writer, String baseName, String className, String fieldList){
     writer.println("  static class " + className + " extends " + baseName + " {");
-   }//this is where i stopped for the night!
+    
+    writer.println("    " + className + "(" + fieldList + ") {");
+    //constructer hehe
 
+    String[] fields = fieldsList.split(", ");
+    for(String field : fields){
+        String name = field.split(" ")[1];
+        writer.println("    this." + name + " = " + name + ";");
+        
+    }
+
+    writer.println("    }");
+
+    writer.println();
+    for(String field : fields){
+        writer.println("    final " + field + ";");
+
+    }
+    writer.println(" }");//ok this is were I have stopped for the night! completeling just the simple script to generate all our data classes (with no actions or methods, to then be translated to a more logical language format with synatx trees and productions!)
+    
+
+}
 
 }
