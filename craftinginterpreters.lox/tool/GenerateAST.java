690cc5e55e069b3b0b75347122d6e318dadbfc75
NewtlyCroco <willmitchellbusinessva@gmail.com>
2025-04-05 16:47:17 +0300

    Yay! The Tree regular laguage class is done from our generated files!

--- craftinginterpreters.lox/tool/GenerateAST.java
+++ craftinginterpreters.lox/tool/GenerateAST.java
@@ -1,6 +1,9 @@
+package org.example.tool;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.List;
 
 public class GenerateAST {
    public static void main(String[] args) throws IOException {
@@ -28,17 +31,41 @@ public class GenerateAST {
     writer.println("import java.util.List;");
     writer.println();
     writer.println("abstract class " + baseName + " {");
+
     for(String type: types){
         String className = type.split(":")[0].trim();
         String fields = type.split(":")[1].trim();
         defineType(writer, baseName, className, fields);
     }
+
     writer.println("}");
     writer.close(); 
    }
-   private static void defineType(PrinterWriter writer, String baseName, String className, String fieldList){
-    writer.println("  static class " + className + " extends " + baseName + " {");
-   }//this is where i stopped for the night!
 
+
+
+   private static void defineType(PrintWriter writer, String baseName, String className, String fieldList){
+
+    writer.println("  static class " + className + " extends " + baseName + " {");
+
+    writer.println("    " + className + "(" + fieldList + ") {"); //This is the constructor class
+    String[] fields = fieldList.split(", ");
+    for( String field: fields){
+        String name = field.split(" ")[1];
+        writer.println("    this." + name + " = " + name + ";");
+    }    
+    
+    writer.println("    }");
+
+    writer.println();
+    for(String field : fields){
+        writer.println("    final " + field + ";");
+    }
+
+    writer.println("  }");
+
+
+
+    }
 
 }
